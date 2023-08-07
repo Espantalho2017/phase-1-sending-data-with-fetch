41a17db315f39b1e152ab21c47e4c220162a4807
Roscoe Moore <rmoore@peersat.com>
2023-08-07 10:37:41 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Add your code here
+
+// In index.js, write a function, submitData, 
+// that takes two strings as arguments, 
+// one representing a user's name and 
+// the other representing a user's email.
+
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+//   };
+
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json",
+//     },
+//     body: JSON.stringify(formData) ,
+// };
+
+function submitData(userName, userEmail) {
+    const formData = {
+        name: userName,
+        email: userEmail,
+      };
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify(formData) ,
+    };
+    return fetch('http://localhost:3000/users', configurationObject)
+        .then(function (response) {
+        return response.json();
+        })
+        .then(function (object) {
+        const id = object.id;
+        document.body.append(id);
+        })
+        .catch(function (error) {
+        alert("Bad things! Ragnarők!");
+        const message = error.message ;
+        document.body.append(message);
+        });    
+} 
+
+submitData() ;
---
+++ test.js
@@ -0,0 +1,27 @@
+
+
+// let total ;
+
+
+function add(x , y) {
+    console.log(x + y) ;
+}
+
+const mult = function multiply(x , y) {
+    let total = x * y ;
+    // console.log(total);
+    // return total;
+}
+
+function divide(x , y) {
+    const div = x/y
+    console.log(div);
+    return div ;
+}
+
+add(2, 3);
+console.log(multiply(3, 4)) ;
+divide(3, 6);
+
+multiply(3, 3) ;
+console.log(total);
